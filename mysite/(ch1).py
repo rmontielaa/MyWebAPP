1536ebd58140579db1fbd23133af2f72155402a3
rmontielaa <21080108@upemor.edu.mx>
2022-09-24 09:50:31 -0500

    Primer commit proyecto polls django

---
+++ mysite/(ch1).py
@@ -0,0 +1,47 @@
+# coding: utf-8
+Question.objects.all()
+query=Question.objects.all()
+print(query.query)
+timezone.datetime
+timezone.datetime.today()
+today=timezone.datetime.today()
+q=('Encuesta 2',today,True,2)
+print(query.1)
+print(query.q)
+type (q)
+q=Question('Encuesta 2',today,True,2)
+type (q)
+q.save()
+q=Question(2.'Encuesta 2',today,True,2)
+q=Question(2,'Encuesta 2',today,True,2)
+q.save()
+Question.objects.all()
+query=Question.objects.all()
+query
+query.count
+query.count()
+query.all()
+q1=query.first()
+q1
+q2=query.last()
+q2
+query.all()
+query=Question.objects.all()
+query
+for q < query:
+    print(q.status)
+for q in query:
+    print(q.status)
+    
+for q in query:
+    print(q.status)
+    
+query=Question.objects.all()
+for q in query:
+    print(q.status)
+    
+ch1=choise(q2,'choise 1 from question 2',10)
+ch1=Choise(1,q2,'choise 1 from question 2',10)
+ch1
+get_ipython().run_line_magic('save', '(ch1)')
+ch1=Choise(1,2,'choise 1 from question 2',10)
---
+++ mysite/polls/models.py
@@ -0,0 +1,22 @@
+from operator import mod
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+class Question (models.Model):
+    question_text= models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+    status =models.BooleanField(default=True)
+    orden= models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.question_text
+    
+class Choise(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
+    

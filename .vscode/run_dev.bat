262dce56af61019df15ebeca1eaf5528cb3731cf
rmontielaa <21080108@upemor.edu.mx>
2022-10-08 12:31:17 -0500

    un commit más final main

---
+++ .vscode/run_dev.bat
@@ -0,0 +1,9 @@
+@echo off
+set DEBUG=true
+set DJANGO_SECRET_KEY=mDMWU2bWCh2THffQZa2TKPcGuX6OcK28g9hQOtTLDsQ6czYuRs
+
+if not defined VIRTUAL_ENV (
+    call venv\Scripts\activate.bat
+)
+
+python mysite\manage.py runserver

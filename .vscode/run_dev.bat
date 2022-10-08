ca548240fb69edacfa6abb77fedb558b8434e101
rmontielaa <21080108@upemor.edu.mx>
2022-10-08 13:45:55 -0500

    un commit primer intento

--- .vscode/run_dev.bat
+++ .vscode/run_dev.bat
@@ -1,6 +1,11 @@
 @echo off
 set DEBUG=true
 set DJANGO_SECRET_KEY=mDMWU2bWCh2THffQZa2TKPcGuX6OcK28g9hQOtTLDsQ6czYuRs
+set NAME=ejemplo
+set USER=postgres
+set PASSWORD=example
+set HOST=127.0.0.1
+set PORT=5432
 
 if not defined VIRTUAL_ENV (
     call venv\Scripts\activate.bat
